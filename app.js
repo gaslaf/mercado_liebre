4f1df061c5ff2c147fb0c3976907841149c54934
gaslaf <lafuentegaston.gl@gmail.com>
2022-10-06 19:39:04 -0300

    agregando login/register

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
 
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'views','home.html'));
-})
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res)=> res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res)=> res.sendFile(path.join(__dirname,'views','login.html')))
